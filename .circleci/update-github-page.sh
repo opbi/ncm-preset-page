09ecaaedf9e42095f155dcb637dffd3dd41e0c5b
Zhenyu Lin <zhenyu.lyn@gmail.com>
2022-09-03 15:20:54 +0100

    chore: remove unrelated files

--- .circleci/update-github-page.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-git config --global user.name 'CircleCI';
-git config --global user.email 'circleci@users.noreply.github.com';
-npx documentation build src/** -f html -o docs
-npx gh-pages -d docs -m 'Automated Github Page Update [skip ci]';
